10d32dcfe0b4f12d4d9e8561207deab93751e515
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-08 14:27:13 +0700

    Add - Safe area, padding, column utama, dan text

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -11,8 +11,20 @@ class TodoPage extends StatelessWidget {
     final _key = GlobalKey<FormState>();
     final _controller = TextEditingController();
     return Scaffold (
+      body: SafeArea(
+        child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text('Todo List'),
+          ],
+        ),
+        
+        )
+        
+        ),
 
-      
+
     );
   }
 }

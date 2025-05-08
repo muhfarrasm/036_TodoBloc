b8fd258f883ea497f94319c1d2a389e555182fe5
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-08 14:19:58 +0700

    Create - Todo Page

---
+++ lib/presentation/todo_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:todobloc/bloc/todo_bloc_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ();
+  }
+}

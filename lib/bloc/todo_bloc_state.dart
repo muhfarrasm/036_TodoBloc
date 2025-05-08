000f8eef858a92f3d4f7367df4ff579b65c88efb
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-08 13:50:28 +0700

    Add - todo_bloc state

--- lib/bloc/todo_bloc_state.dart
+++ lib/bloc/todo_bloc_state.dart
@@ -4,3 +4,13 @@ part of 'todo_bloc_bloc.dart';
 sealed class TodoBlocState {}
 
 final class TodoBlocInitial extends TodoBlocState {}
+final class TodoBlocLoading extends TodoBlocState {}
+
+final class TodoBlocLoaded extends TodoBlocState {
+  final List<Todo> todos;
+  DateTime? selectedDate;
+
+  TodoBlocLoaded({
+    required this.todos, required this.selectedDate
+  });
+}

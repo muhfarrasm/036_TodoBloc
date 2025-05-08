04bd20b4706c8b8433e8246b655d1572f959c9ec
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-08 14:50:05 +0700

    Add - Show date Picker

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -21,8 +21,8 @@ class TodoPage extends StatelessWidget {
                   Text('Selected Data'),
                   BlocBuilder<TodoBlocBloc, TodoBlocState>(
                     builder: (context, state) {
-                      if (state is TodoBlocLoaded){
-                        if (state.selectedDate != null){
+                      if (state is TodoBlocLoaded) {
+                        if (state.selectedDate != null) {
                           return Text(
                             '${state.selectedDate!.day}/${state.selectedDate!.month}/${state.selectedDate!.year}',
                           );
@@ -30,9 +30,29 @@ class TodoPage extends StatelessWidget {
                       }
                       return Text('No date selected');
                     },
-                    
                   ),
                 ],
+              ),
+
+              SizedBox(width: 16.0),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {
+                    showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2100),
+                    ).then((selectedDate) {
+                      if (selectedDate != null) {
+                        context.read<TodoBlocBloc>().add(
+                          TodoSelectDate(date: selectedDate),
+                        );
+                      }
+                    });
+                  },
+                  child: Text('Select Date'),
+                ),
               ),
             ],
           ),

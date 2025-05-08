44a4b46a414197434196adf95189e37d47478522
muhfarrasm <muhammadfarrasm@gmail.com>
2025-05-08 13:45:48 +0700

    Add - bloc evet

--- lib/bloc/todo_bloc_event.dart
+++ lib/bloc/todo_bloc_event.dart
@@ -1,4 +1,23 @@
 part of 'todo_bloc_bloc.dart';
 
-@immutable
+
 sealed class TodoBlocEvent {}
+final class TodoEventAdd extends TodoBlocEvent {
+  final String title;
+  final String date;
+  TodoEventAdd({required this.title,required this.date });
+
+
+}
+
+final class TodoEventComplete extends TodoBlocEvent{
+  final int index;
+
+  TodoEventComplete({required this.index});
+}
+
+final class TodoSelectDate extends TodoBlocEvent{
+  final DateTime date;
+
+  TodoSelectDate({required this.date});
+}
